09505e4e53e6fbb97bc5e7def15d5c2728c28b81
tuttuwucha <tuttuwucha@gmail.com>
2026-07-21 02:42:36 +1000

    modified:   fuck_diamonds.js

--- fuck_diamonds.js
+++ fuck_diamonds.js
@@ -1,8 +1,23 @@
 LootJS.modifiers(event => {
+    const fortune = LootEntry.of("minecraft:coal")
+    .applyOreBonus("minecraft:fortune")
+    .when(c => c.matchMainHand(ItemFilter.hasEnchantment("minecraft:fortune")))
+
+    const whiteSilkTouch = LootEntry.of("minecraft:coal_ore")
+    .when(c => c.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")))
+
+    const blackSilkTouch = LootEntry.of("minecraft:deepslate_coal_ore")
+    .when(c => c.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch")))
+
     event.addBlockModifier("minecraft:diamond_ore")
-    .replaceLoot("minecraft:diamond", "minecraft:coal")
+    .removeLoot("minecraft:diamond")
+    .removeLoot("minecraft:diamond_ore")
+    .addAlternativesLoot(fortune, whiteSilkTouch, "minecraft:coal")
 
 
     event.addBlockModifier("minecraft:deepslate_diamond_ore")
-    .replaceLoot("minecraft:diamond", "minecraft:coal")
+    .removeLoot("minecraft:diamond")
+    .removeLoot("minecraft:deepslate_diamond_ore")
+    .addAlternativesLoot(fortune, blackSilkTouch, "minecraft:coal")
+
 })
